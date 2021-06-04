28d3de925d86922c69e461bb64cb879225af67ca
taylornjohnson <johnson.taylornicole@gmail.com>
2021-06-04 16:46:46 -0400

    repairs

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -4,18 +4,18 @@ var tableData = data;
 // YOUR CODE HERE!
 //table reference
 var tbody = d3.select("tbody");
-function buildtable(data) {
+function buildTable(data) {
 
     tbody.html("");
     // loop through data
-    data.foreach((datarow) => {
+    data.forEach((datarow) => {
         var row = tbody.append("tr");
 
-        Object.values(datarow).foreach((val) => {
+        Object.values(datarow).forEach((val) => {
             var cell = row.append("td");
             cell.text(val);
         });
     });
 }
 // build table 
-buildtable(tableData);
+buildTable(tableData);
